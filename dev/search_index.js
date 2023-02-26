c9965e9c758956ca89a0d03c18a892778190d83c
Documenter.jl <documenter@juliadocs.github.io>
2023-02-26 07:56:15 +0000

    build based on 99bdad1

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = JuMagFE","category":"page"},{"location":"#JuMagFE","page":"Home","title":"JuMagFE","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for JuMagFE.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [JuMagFE]","category":"page"}]
+}
